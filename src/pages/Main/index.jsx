c732e7523207075074dc89e1bda29cd7d3ce0ec1
LeeYooBin <santosroberdan25@gmail.com>
2022-07-21 20:35:47 -0300

    Configurações iniciais

---
+++ src/pages/Main/index.jsx
@@ -0,0 +1,21 @@
+import * as S from '../../style/Global';
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route
+} from "react-router-dom";
+import Home from '../Home';
+
+export default function Main(){
+    return(
+        <>
+            <Router>
+                <S.GlobalStyle />
+
+                <Routes>
+                    <Route path='/' element={<Home />}></Route>
+                </Routes>
+            </Router>
+        </>
+    );
+}
